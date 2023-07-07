2f24b67ca2f5fba19993f98b0d5f3906a5d1ee4d
Lía Da Silva Rojas <lia.dasilva.info@gmail.com>
2023-07-06 21:52:05 -0400

    cambios 2.0

--- TiendaPetShop/core/urls.py
+++ TiendaPetShop/core/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', home, name="home"),
     path('producto/<action>/<id>', producto, name="producto"),
     path('producto/tienda', producto_tienda, name="producto_tienda"),
-    path('producto_ficha/<id>', producto_ficha, name="producto_ficha"),
+    path('producto_ficha/<id>', producto_ficha, name="ficha"),
 ]
 
