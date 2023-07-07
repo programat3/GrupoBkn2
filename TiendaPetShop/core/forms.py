324132e19374e79241ea926bae6ddb9f37e6e5f1
Lía Da Silva Rojas <lia.dasilva.info@gmail.com>
2023-07-06 21:36:38 -0400

    cambios de acuerdo al instructivo

--- TiendaPetShop/core/forms.py
+++ TiendaPetShop/core/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm, fields
 from .models import Producto
 
 
-class VehiculoForm(ModelForm):
+class ProductoForm(ModelForm):
     class Meta:
         model = Producto
         fields = ['idProducto', 'nombreProducto', 'precioProducto', 'stockProducto', 'imagenProducto', 'categoria']
--- TiendaPetShop/core/views.py
+++ TiendaPetShop/core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render
+from .models import Categoria, Producto, Usuario, Compra
+from .forms import ProductoForm
 
 # Create your views here.
+
+def home(request):
+    return render(request, "core/index.html")
+
+def producto_tienda(request):
+    data = {"list": Producto.objects.all().order_by('idProducto')}
+    return render(request, "core/productos.html", data)
+
+def producto_ficha(request, id):
+    producto = producto.objects.get(patente=id)
+    data = {"producto":  producto}
+    return render(request, "core/ficha.html", data)
+
