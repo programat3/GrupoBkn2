b1854e4b746ef4ab61232bdf9aea3c2b703d9f06
Lía Da Silva Rojas <lia.dasilva.info@gmail.com>
2023-07-10 14:50:44 -0400

    cambios bases de datos y mantenedor de productos funcionando

--- TiendaPetShop/core/urls.py
+++ TiendaPetShop/core/urls.py
@@ -1,22 +1,46 @@
 from django.urls import path
-from .views import home, productos, producto_tienda, ficha, nosotros, administracion, bodega, boleta, carrito, historial_ventas, ingresar, mis_compras, misdatos, registro, ropa, usuarios
+from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
+from .views import index, registrarme, nosotros, admin_productos
+from .views import admin_usuarios, admin_bodega, ventas, boleta, ingresar
+from .views import misdatos, miscompras, salir, carrito, ficha
+from .views import cambiar_estado_boleta, poblar
+from .views import obtener_productos, eliminar_producto_en_bodega, premio, eliminar_producto_en_carrito, agregar_producto_al_carrito
 
 urlpatterns = [
-    path('', home, name="home"),
-    path('productos/<action>/<id>', productos, name="productos"),
-    path('producto/tienda', producto_tienda, name="producto_tienda"),
-    path('ficha/<id>', ficha, name="ficha"),
-    path('nosotros', nosotros, name="nosotros"),
-    path('administracion', administracion, name='administracion'),
-    path('bodega', bodega, name='bodega'),
-    path('boleta', boleta, name='boleta'),
-    path('carrito', carrito, name='carrito'),
-    path('historial_ventas', historial_ventas, name='historial_ventas'),
+    path('', index, name='index'),
+    path('registrarme', registrarme, name='registrarme'),
+    path('nosotros', nosotros, name='nosotros'),
+    
+    #path('admin_productos/<accion>/<producto_id>', admin_productos, name='admin_productos'),
+    path('admin_productos/<accion>/<id>', admin_productos, name='admin_productos'),
+
+    #path('eliminar_producto/<pk>', eliminar_producto, name='eliminar_producto'),
+    path('admin_usuarios', admin_usuarios, name='admin_usuarios'),
+    
+    path('admin_bodega', admin_bodega, name='admin_bodega'),
+    path('obtener_productos', obtener_productos, name='obtener_productos'),
+    path('eliminar_producto_en_bodega/<bodega_id>', eliminar_producto_en_bodega, name='eliminar_producto_en_bodega'),
+
+
+    path('ventas', ventas, name='ventas'),
+    path('boleta/<nro_boleta>', boleta, name='boleta'),
+    path('cambiar_estado_boleta/<nro_boleta>/<estado>', cambiar_estado_boleta, name='cambiar_estado_boleta'),
     path('ingresar', ingresar, name='ingresar'),
-    path('mis_compras', mis_compras, name='mis_compras'),
     path('misdatos', misdatos, name='misdatos'),
-    path('registro', registro, name='registro'),
-    path('ropa', ropa, name='ropa'),
-    path('usuarios', usuarios, name='usuarios'),
+    path('miscompras', miscompras, name='miscompras'),
+    path('salir', salir, name='salir'),
+    path('carrito', carrito, name='carrito'),
+    path('eliminar_producto_en_carrito/<carrito_id>', eliminar_producto_en_carrito, name='eliminar_producto_en_carrito'),
+    path('agregar_producto_al_carrito/<producto_id>', agregar_producto_al_carrito, name='agregar_producto_al_carrito'),
+    path('ficha/<producto_id>', ficha, name='ficha'),
+    path('premio', premio, name='premio'),
+    path('poblar', poblar, name='poblar'),
+
+    # path('cerrarsesion', cerrarsesion, name='cerrarsesion'),
+    # path('pswcambiada', TemplateView.as_view(template_name='core/pswcambiada.html'), name='pswcambiada'),
+    # path('cambiarpsw', auth_views.PasswordChangeView.as_view(template_name='core/cambiarpsw.html', success_url='/cambiarpsw'), name='cambiarpsw'),
+    # path('pagar/<id>', pagar, name="pagar"),
+    # path('pagoexitoso/', pagoexitoso, name="pagoexitoso"),
 ]
 
