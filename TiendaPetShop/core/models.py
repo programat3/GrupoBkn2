d9e03c71ed8be477c6f8d384e4b6c6e3b50cde93
Lía Da Silva Rojas <lia.dasilva.info@gmail.com>
2023-07-10 12:15:06 -0400

    lo que el profe dio como indicacion

--- TiendaPetShop/core/models.py
+++ TiendaPetShop/core/models.py
@@ -1,39 +1,140 @@
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-#Create Modelo Para Categoría
 class Categoria(models.Model):
-    idCategoria = models.IntegerField(primary_key=True, verbose_name="Id de categoría")
-    nombreCategoria = models.CharField(max_length=80, blank=False, null=False, verbose_name="Nombre de la categoría")
-
+    id_categoria = models.IntegerField(primary_key=True, blank=False, null=False, verbose_name='ID Categoría')
+    nombre = models.CharField(max_length=100, blank=False, null=False, verbose_name='Nombre categoría')
+    
+    class Meta:
+        db_table = 'Categoria'
+    
     def __str__(self):
-        return self.nombreCategoria
+        return f'{self.id_categoria} - {self.nombre}'
 
 class Producto(models.Model):
-    idProducto = models.IntegerField(primary_key=True, verbose_name="Id de producto")
-    nombreProducto = models.CharField(max_length=80, blank=False, null=False, verbose_name="nombre del producto")
-    precioProducto = models.IntegerField(blank=False, null=False, verbose_name="Precio del producto")
-    descripcionProducto = models.CharField(max_length=200, blank= False, null=False, verbose_name="Descripción del producto")
-    stockProducto = models.IntegerField(blank=False, null=False, verbose_name="Stock del Producto")
-    imagenProducto = models.ImageField(upload_to="images/", default="sinfoto.jpg", null=False, blank=False, verbose_name="Imagen del Producto")
-    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    id_producto = models.IntegerField(primary_key=True, blank=False, null=False, verbose_name='ID Producto')
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, verbose_name='Categoría')
+    nombre = models.CharField(max_length=100, blank=False, null=False, verbose_name='Nombre producto')
+    descripcion = models.CharField(max_length=400, blank=False, null=False, verbose_name='Descripción')
+    precio = models.IntegerField(blank=False, null=False, verbose_name='Precio')
+    descuento_subscriptor = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        blank=False,
+        null=False,
+        verbose_name='Descuento subscriptor %'
+    )
+    descuento_oferta = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        blank=False,
+        null=False,
+        verbose_name='Descuento oferta %'
+    )
+    imagen = models.ImageField(upload_to='productos/', blank=False, null=False, verbose_name='Imagen')
+    
+    class Meta:
+        db_table = 'Producto'
 
     def __str__(self):
-        return self.nombreProducto
+        return f'{self.id_producto} - {self.categoria.nombre} - {self.nombre}'
 
-class Usuario(models.Model):
-    idUsuario = models.IntegerField(primary_key=True, verbose_name="Id de usuario")
-    nombreUsuario = models.CharField(max_length=80, blank=False, null=False, verbose_name="nombre de usuario")
-    mail = models.CharField(max_length=80, blank=False, null=False, verbose_name="correo electrónico")
-
-    def __str__(self):
-        return self.nombreUsuario
-
-class Compra(models.Model):
-    idCompra = models.IntegerField(primary_key=True, verbose_name="Id de compra")
-    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+class Carrito(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.DO_NOTHING)
-    estado = models.IntegerField(blank=False, null=False,verbose_name="Estado de compra")
+    precio = models.IntegerField(blank=False, null=False, verbose_name='Precio')
+    descuento_subscriptor = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        blank=False,
+        null=False,
+        verbose_name='Descto subs'
+    )
+    descuento_oferta = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        blank=False,
+        null=False,
+        verbose_name='Descto oferta'
+    )
+    descuento_total = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        blank=False,
+        null=False,
+        verbose_name='Descto total'
+    )
+    descuentos = models.IntegerField(blank=False, null=False, verbose_name='Descuentos')
+    precio_a_pagar = models.IntegerField(blank=False, null=False, verbose_name='Precio a pagar')
+    class Meta:
+        db_table = 'Carrito'
 
-    def __str__(self):
-        return self.idCompra
+class Perfil(models.Model):
+    USUARIO_CHOICES = [
+        ('Cliente', 'Cliente'),
+        ('Administrador', 'Administrador'),
+        ('Superusuario', 'Superusuario'),
+    ]
+    idusu = models.IntegerField(primary_key=True, blank=False, null=False, verbose_name='ID Usuario')
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    tipo_usuario = models.CharField(
+        choices=USUARIO_CHOICES,
+        max_length=50,
+        blank=False,
+        null=False,
+        verbose_name='Tipo de usuario'
+    )
+    rut = models.CharField(max_length=15, blank=False, null=False, verbose_name='RUT')
+    direccion = models.CharField(max_length=400, blank=False, null=False, verbose_name='Dirección')
+    subscrito = models.BooleanField(blank=False, null=False, verbose_name='Subscrito')
+    imagen = models.ImageField(upload_to='perfiles/', blank=False, null=False, verbose_name='Imagen')
+    class Meta:
+        db_table = 'Perfil'
 
+class Boleta(models.Model):
+    ESTADO_CHOICES = [
+        ('Anulado', 'Anulado'),
+        ('Vendido', 'Vendido'),
+        ('Despachado', 'Despachado'),
+        ('Entregado', 'Entregado'),
+    ]
+    cliente = models.ForeignKey(Perfil, on_delete=models.DO_NOTHING)
+    monto_sin_iva = models.IntegerField(blank=False, null=False, verbose_name='Monto sin IVA')
+    iva = models.IntegerField(blank=False, null=False, verbose_name='IVA')
+    total_a_pagar = models.IntegerField(blank=False, null=False, verbose_name='Total a pagar')
+    fecha_venta = models.DateField(blank=False, null=False, verbose_name='Fecha de venta')
+    fecha_despacho = models.DateField(blank=False, null=False, verbose_name='Fecha de despacho')
+    fecha_entrega = models.DateField(blank=False, null=False, verbose_name='Fecha de entrega')
+    estado = models.CharField(choices=ESTADO_CHOICES, max_length=50, blank=False, null=False, verbose_name='Estado')
+    class Meta:
+        db_table = 'Boleta'
+
+class DetalleBoleta(models.Model):
+    boleta = models.ForeignKey(Boleta, on_delete=models.DO_NOTHING)
+    numero_item = models.IntegerField(blank=False, null=False, verbose_name='Número de item')
+    producto = models.ForeignKey(Producto, on_delete=models.DO_NOTHING)
+    precio = models.IntegerField(blank=False, null=False, verbose_name='Precio')
+    descuento_subscriptor = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        blank=False,
+        null=False,
+        verbose_name='Descto subs'
+    )
+    descuento_oferta = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        blank=False,
+        null=False,
+        verbose_name='Descto oferta'
+    )
+    descuento_total = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        blank=False,
+        null=False,
+        verbose_name='Descto total'
+    )
+    descuentos = models.IntegerField(blank=False, null=False, verbose_name='Descuentos')
+    precio_a_pagar = models.IntegerField(blank=False, null=False, verbose_name='Precio a pagar')
+    class Meta:
+        db_table = 'DetalleBoleta'
+
+class Bodega(models.Model):
+    id_bodega = models.IntegerField(primary_key=True, blank=False, null=False, verbose_name='ID Bodega')
+    producto = models.ForeignKey(Producto, on_delete=models.DO_NOTHING, verbose_name='Producto')
+    boleta = models.ForeignKey(Boleta, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Boleta')
+    class Meta:
+        db_table = 'Bodega'
